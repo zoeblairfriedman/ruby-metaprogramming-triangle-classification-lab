ea1b7a253b3852a85e298e286b866c3cf8a2d153
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-28 21:49:33 -0500

    commit tree 61a995d83c9da6b39a04bcd48a009125875be620

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+attr_accessor :side_3, :side_2, :side_1
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def is_triangle?
+    if side_1 + side_2 > side_3 && side_2 + side_3 > side_1 && side_3 + side_1 > side_2
+      true
+    else
+      false
+    end
+  end
+
+  def kind
+    if !is_triangle?
+      raise TriangleError
+    elsif
+      side_1 == side_2 && side_2 == side_3
+        :equilateral
+    elsif
+      #isos
+      side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+        :isosceles
+    else
+        :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    "This is not a valid Triangle"
+  end
+
 end
